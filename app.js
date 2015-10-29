d3a579e1252aead41fb287754467a8b3b01fedc7
herotest <herotest@gmail.com>
2015-10-29 05:38:41 +0000

    changing test script

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('hello jenkinsss');
+  res.send('hello jenkins');
 });
 
-app.listen(process.env.PORT || 5000);
+app.listen(process.env.PORT || 6000);
 
 module.exports = app;
